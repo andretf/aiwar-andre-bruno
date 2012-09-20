953dd178a3f2e964f669faba312d557095c59943
andretf <andretf@gmail.com>
2012-09-20 22:17:19 +0000

    add do projeto no Svn

---
+++ AIWar/Core/GameObjectCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIWar.Core
+{
+    class GameObjectCollection
+    {
+        protected List<GameObject> Lista;
+
+        public GameObjectCollection(){
+            this.Lista.Add(new GameObject(1, "tokenBlackNeutron"));
+            this.Lista.Add(new GameObject(2, "tokenBlackEletron"));
+            this.Lista.Add(new GameObject(3, "tokenBlackPositron"));
+            this.Lista.Add(new GameObject(4, "tokenWhiteNeutron"));
+            this.Lista.Add(new GameObject(5, "tokenWhiteEletron"));
+            this.Lista.Add(new GameObject(6, "tokenWhitePositron"));
+        }
+
+        public System.Drawing.Image GetImage(int id)
+        {
+            return (from img in Lista
+                    where img.ID = id
+                    select Properties.Resources.ResourceManager.GetObject(img.Image));
+        }
+    }
+}
