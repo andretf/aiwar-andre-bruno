18fece43e512b5585203009688debd7e8d0d5c5c
andretf@gmail.com <andretf@gmail.com@331985a9-da23-5738-719d-aa8c4c3e0f0a>
2012-11-02 17:51:13 +0000

    Peças e Tabuleiro OK!

--- trunk/AIWar/Form1.Designer.cs
+++ trunk/AIWar/Form1.Designer.cs
@@ -28,37 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // panel1
             // 
-            this.pictureBox1.Image = global::AIWar.Properties.Resources.tabuleiro;
-            this.pictureBox1.InitialImage = global::AIWar.Properties.Resources.tabuleiro;
-            this.pictureBox1.Location = new System.Drawing.Point(30, 25);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(324, 403);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::AIWar.Properties.Resources.tabuleiro;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 403);
+            this.panel1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(815, 600);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- trunk/AIWar/Form1.cs
+++ trunk/AIWar/Form1.cs
@@ -56,12 +56,11 @@ namespace AIWar
         }
 
         protected bool Draw() {
-            pictureBox1.Hide();
             for (int i = 0; i < 21; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    if (GameMatrix[i, j] >= 0) {
+                    if (GameMatrix[i, j] > 0) {
                         int offsetLeft = 0;
                         int offsetTop = 0;
                         if (i % 2 == 0)
@@ -76,24 +75,17 @@ namespace AIWar
                             offsetLeft += 0;
                             offsetLeft += j * 56;
                             offsetTop += 18;
-                            offsetTop += (i-1) * 36;
+                            offsetTop += (int)i/2 * 36;
                         }
                         
                         PictureBox e = new PictureBox();
-                        e.Parent = pictureBox1;
+                        e.Parent = panel1;
                         e.BackColor = System.Drawing.Color.Transparent;
                         e.Margin = new Padding(0);
                         e.TabIndex = 0;
-                        if (GameMatrix[i, j] == 0){
-                            e.Size = new Size(40, 38);
-                            e.Location = new Point(offsetLeft + pictureBox1.Left,
-                                                    offsetTop + pictureBox1.Top);
-                        }
-                        else{
-                            e.Size = new Size(15, 15);
-                            e.Location = new Point(offsetLeft + pictureBox1.Left + 8,
-                                                    offsetTop + pictureBox1.Top + 8);
-                        }
+                        e.Size = new Size(15, 15);
+                        e.Location = new Point(offsetLeft + panel1.Left,
+                                                offsetTop + panel1.Top);
                         switch(GameMatrix[i, j]){
                             case 1: e.Image = Resources.tokenBlackNeutron; break;
                             case 2: e.Image = Resources.tokenBlackEletron; break;
@@ -101,11 +93,10 @@ namespace AIWar
                             case 4: e.Image = Resources.tokenWhiteNeutron; break;
                             case 5: e.Image = Resources.tokenWhiteEletron; break;
                             case 6: e.Image = Resources.tokenWhitePositron; break;
-                            default: e.Image = Resources.empty; break;
                         }
                         
                         
-                        this.Controls.Add(e);
+                        this.panel1.Controls.Add(e);
                     }
                     
 
