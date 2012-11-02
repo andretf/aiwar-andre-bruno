215d91eb74a63c477e2605840e168e495d43dc8b
andretf <andretf@gmail.com>
2012-11-02 23:55:08 +0000

    Movido de matriz pra vetor adicionado metodos de calculo

--- AIWar/Form1.cs
+++ AIWar/Form1.cs
@@ -13,7 +13,9 @@ namespace AIWar
 {
     public partial class Form1 : Form
     {
-        protected int[,] GameMatrix = new int[21, 6] {
+        #region "Properties, Globals"
+        [Obsolete]
+        public int[,] GameMatrix = new int[21, 6] {
             {  0, 0, 1, 0, 0, 0 },
             {0, 0, 3, 3, 0, 0 },
             {  0, 2, 2, 2, 0, 0 },
@@ -36,35 +38,96 @@ namespace AIWar
             {0, 0, 6, 6, 0, 0 },
             {  0, 0, 4, 0, 0, 0 }
         };
-        protected PictureBox[,] GamePictureMatrix = new PictureBox[21, 6];
+        public int[] TabuleiroVetor = new int[115] {
+              0, 0, 1, 0, 0, 
+            0, 0, 3, 3, 0, 0,
+              0, 2, 2, 2, 0, 
+            0, 0, 3, 3, 0, 0,
+              0, 0, 2, 0, 0, 
+            0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 
+            0, 0, 6, 6, 0, 0,
+              0, 5, 5, 5, 0, 
+            0, 0, 6, 6, 0, 0,
+              0, 0, 4, 0, 0
+        };
+
         protected int ObjectsSize = 15;
         private GameObjectCollection Pecas;
-        
+        #endregion
+
+        #region "Initializers"
         public Form1()
         {
             InitializeComponent();
             initGame();
         }
+        #endregion
 
+        #region "Main Methods"
         protected bool initGame()
         {
             Pecas = new GameObjectCollection();
 
-            if (Draw())
+            if (desenhaPecas())
                 return true;
             return false;
         }
 
+        protected bool desenhaPecas() {
+            for (int i = 0; i < 115; i++) {
+                if (TabuleiroVetor[i] > 0) {
+                    PictureBox e = new PictureBox();
+                    e.Parent = panel1;
+                    e.BackColor = System.Drawing.Color.Transparent;
+                    e.Margin = new Padding(0);
+                    e.TabIndex = 0;
+                    e.Size = new Size(15, 15);
+                    e.Location = new Point(Positions.VetorPeca[i, 0] + panel1.Left, Positions.VetorPeca[i, 1] + panel1.Top);
+
+                    switch(TabuleiroVetor[i]){
+                        case 1: e.Image = Resources.tokenBlackNeutron; break;
+                        case 2: e.Image = Resources.tokenBlackEletron; break;
+                        case 3: e.Image = Resources.tokenBlackPositron; break;
+                        case 4: e.Image = Resources.tokenWhiteNeutron; break;
+                        case 5: e.Image = Resources.tokenWhiteEletron; break;
+                        case 6: e.Image = Resources.tokenWhitePositron; break;
+                    }
+                        
+                    this.panel1.Controls.Add(e);
+                }
+            }
+            return false;
+        }
+        #endregion
+
+
+
+        [Obsolete]
         protected bool Draw() {
+            int index = 0;
+            List<string> lines = new List<string>();
+
             for (int i = 0; i < 21; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 6; j++)
                 {
-                    if (GameMatrix[i, j] > 0) {
+                    if (GameMatrix[i, j] >= 0) {
                         int offsetLeft = 0;
                         int offsetTop = 0;
                         if (i % 2 == 0)
                         {
+                            if (j == 5) break;
                             offsetLeft += 28;               // Offset Fixo
                             offsetLeft += j * 56;           // Offset Dinamicamo: i * k
                             offsetTop += 0;                 // Offset Fixo
@@ -75,7 +138,7 @@ namespace AIWar
                             offsetLeft += 0;
                             offsetLeft += j * 56;
                             offsetTop += 18;
-                            offsetTop += (int)i/2 * 36;
+                            offsetTop += (int)(i/2) * 36;
                         }
                         
                         PictureBox e = new PictureBox();
@@ -86,6 +149,7 @@ namespace AIWar
                         e.Size = new Size(15, 15);
                         e.Location = new Point(offsetLeft + panel1.Left,
                                                 offsetTop + panel1.Top);
+
                         switch(GameMatrix[i, j]){
                             case 1: e.Image = Resources.tokenBlackNeutron; break;
                             case 2: e.Image = Resources.tokenBlackEletron; break;
@@ -95,7 +159,6 @@ namespace AIWar
                             case 6: e.Image = Resources.tokenWhitePositron; break;
                         }
                         
-                        
                         this.panel1.Controls.Add(e);
                     }
                     
@@ -103,9 +166,7 @@ namespace AIWar
                 }
             }
 
-
             return false;
         }
-
     }
 }
