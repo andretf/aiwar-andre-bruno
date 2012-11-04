e8519589a55128b863e56bc66a568c7ebaf69d01
andretf <andretf@gmail.com>
2012-11-04 01:57:48 +0000

    ...

--- AIWar/Form1.Designer.cs
+++ AIWar/Form1.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblJogada = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -36,19 +45,93 @@
             this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.BackgroundImage = global::AIWar.Properties.Resources.tabuleiro;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(15, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(324, 403);
+            this.panel1.Size = new System.Drawing.Size(375, 403);
             this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(396, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Jogada:";
+            // 
+            // lblJogada
+            // 
+            this.lblJogada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblJogada.Location = new System.Drawing.Point(447, 13);
+            this.lblJogada.Name = "lblJogada";
+            this.lblJogada.Size = new System.Drawing.Size(100, 23);
+            this.lblJogada.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Novo Jogo!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(11, 10);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(597, 69);
+            this.panel2.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblJogada);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Location = new System.Drawing.Point(11, 85);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(597, 429);
+            this.panel3.TabIndex = 7;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 15);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "PC";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 38);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "Humano";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(815, 600);
-            this.Controls.Add(this.panel1);
-            this.Name = "AIwar";
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
             this.Text = "AIwar";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +139,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblJogada;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- AIWar/Form1.cs
+++ AIWar/Form1.cs
@@ -11,7 +11,10 @@ using AIWar.Core;
 
 namespace AIWar
 {
-	public partial class Form1 : Form
+    // TODO:
+    // - PC jogador: entre as casas visiveis, escolher uma!
+    // - Minimax
+    public partial class Form1 : Form
 	{
 		#region "Properties, Globals"
         // 1 - Neutron Preto
@@ -20,49 +23,116 @@ namespace AIWar
         // 4 - Neutron Branco
         // 5 - Eletron Branco
         // 6 - Proton Branco
-		public int[] TabuleiroVetor = new int[115] {
-			  0, 0, 1, 0, 0, 
-			0, 0, 3, 3, 0, 0,
-			  0, 2, 2, 2, 0, 
-			0, 0, 3, 3, 0, 0,
-			  0, 0, 2, 0, 0, 
-			0, 0, 0, 0, 0, 0,
-			  0, 0, 0, 0, 0, 
-			0, 0, 0, 0, 0, 0,
-			  0, 0, 0, 0, 0, 
-			0, 0, 0, 0, 0, 0,
-			  0, 0, 0, 0, 0, 
-			0, 0, 0, 0, 0, 0,
-			  0, 0, 0, 0, 0, 
-			0, 0, 0, 0, 0, 0,
-			  0, 0, 0, 0, 0, 
-			0, 0, 0, 0, 0, 0,
-			  0, 0, 5, 0, 0, 
-			0, 0, 6, 6, 0, 0,
-			  0, 5, 5, 5, 0, 
-			0, 0, 6, 6, 0, 0,
-			  0, 0, 4, 0, 0
-		};
-        private enum player { PC, humano };
+        public int[] TabuleiroVetor;
         private enum token {neutronPreto, eletronPreto, protonPreto,
                             neutronBranco, eletronBranco, protonBranco};
 
 		protected int ObjectsSize = 15;
 		private GameObjectCollection Pecas;
-        private player token_jogador = player.PC;
-        private int token_posicao = 0;
+        private player token_jogador;
+        private int token_posicao;
+        private List<player> jogadores;
+        private bool JogoEmAndamento = false;
+        private bool JogadaCompleta = true;
+        const int MAX_VETOR = 137;
 		#endregion
 
-		#region "Initializers"
+#region "Initializers"
 		public Form1()
 		{
 			InitializeComponent();
-			initGame();
-		}
-		#endregion
+            initGame();
+        }
+#endregion
 
-		#region "Events"
-        private void PecaClick(object sender, EventArgs e){
+#region "Events"
+        private void button1_Click(object sender, EventArgs e) {
+            jogadores = new List<player>();
+            //TabuleiroVetor = new int[137]{
+            //    0, 0, 0, 1, 0, 0, 0,
+            //      0, 0, 3, 3, 0, 0,
+            //    0, 0, 2, 2, 2, 0, 0,
+            //      0, 0, 3, 3, 0, 0,
+            //    0, 0, 0, 2, 0, 0, 0,
+            //      0, 0, 0, 0, 0, 0,
+            //    0, 0, 0, 0, 0, 0, 0,
+            //      0, 0, 0, 0, 0, 0,
+            //    0, 0, 0, 0, 0, 0, 0,
+            //      0, 0, 0, 0, 0, 0,
+            //    0, 0, 0, 0, 0, 0, 0,
+            //      0, 0, 0, 0, 0, 0,
+            //    0, 0, 0, 0, 0, 0, 0,
+            //      0, 0, 0, 0, 0, 0,
+            //    0, 0, 0, 0, 0, 0, 0,
+            //      0, 0, 0, 0, 0, 0,
+            //    0, 0, 0, 5, 0, 0, 0,
+            //      0, 0, 6, 6, 0, 0,
+            //    0, 0, 5, 5, 5, 0, 0,
+            //      0, 0, 6, 6, 0, 0,
+            //    0, 0, 0, 4, 0, 0, 0 
+            //};
+
+            TabuleiroVetor = new int[137]{
+                0, 0, 0, 1, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 2, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 5, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 6,
+                0, 0, 0, 3, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0,
+                0, 0, 0, 4, 0, 0, 0 
+		    };
+
+            token_posicao = 0;
+            JogoEmAndamento = true;
+
+            //Draw();
+            criaPecas();
+
+            player p1 = new player();
+            p1.Jogador = Enums.pType.PC;
+            p1.name = "PC";
+
+            player p2 = new player();
+            p2.Jogador = Enums.pType.humano;
+            p2.name = "Humano";
+
+            jogadores.Add(p1);
+            jogadores.Add(p2);
+
+            if(radioButton1.Checked){
+                p1.Cor = Enums.pColor.branca;
+                p2.Cor = Enums.pColor.preta;
+                token_jogador = p1;
+            }
+            else{
+                p2.Cor = Enums.pColor.branca;
+                p1.Cor = Enums.pColor.preta;
+                token_jogador = p2;
+            }
+            
+            lblJogada.Text = token_jogador.name + " - " + token_jogador.Cor.ToString();
+        }
+
+        private void PecaClick(object sender, EventArgs e) {
+            if (!JogoEmAndamento) {
+                MessageBox.Show("Clique em Iniciar jogo.");
+                return;
+            }
+
             PictureBox peca = (PictureBox)sender;
             int position = getIndex(peca);
 
@@ -71,26 +141,93 @@ namespace AIWar
                 return;
             }
 
-            // Se tem alguma peca...
-            if (TabuleiroVetor[position] > 0) {
-                // primeira desmarca tudo
-                redesenhaTabuleiro();
-
-                peca.Image = GetBlinkedImage(position);
-
-                // e então marca aquelas no campo de visao
-                foreach (int i in Core.Core.getCasasVisiveis(TabuleiroVetor, position))
-                    getPeca(i).Image = GetBlinkedImage(i);
+            if ((TabuleiroVetor[position] > 0) &&
+                (token_jogador.Cor != getPecaCor(position)) &&
+                JogadaCompleta) {
+                MessageBox.Show("Clique na sua peça.");
+                return;
             }
-            // Se está vazio
+            if ((TabuleiroVetor[position] == 0) && JogadaCompleta) {
+                MessageBox.Show("Jogada proibida.");
+                return;
+            }
+
+            if (!JogadaCompleta && DeveCapturar()) {
+                if (!PecaQueCaptura(position)) {
+                    MessageBox.Show("A captura é obrigatória.");
+                    return;
+                }
+                else {
+                    if (getPecaCor(position) != Enums.pColor.undefined &&
+                        getPecaCor(position) != token_jogador.Cor) {
+
+                        // exec jogada
+                        if (Core.Core.getPecasVisiveis(TabuleiroVetor, token_posicao).Contains(position)) {
+                            TabuleiroVetor[position] = TabuleiroVetor[token_posicao];
+                            TabuleiroVetor[token_posicao] = 0;
+                            redesenhaTabuleiro();
+                            }
+                        else {
+                            MessageBox.Show("Jogada proibida.");
+                            return;
+                            }
+
+                        if (ExecutaJogada(position))
+                            JogadaCompleta = true;
+                    }
+                    else {
+                        if (getPecaCor(position) == Enums.pColor.undefined) {
+                            MessageBox.Show("A captura é obrigatória.");
+                            return;
+                            }
+                        else {
+                            // primeira desmarca tudo
+                            redesenhaTabuleiro();
+
+                            peca.Image = GetBlinkedImage(position);
+                            JogadaCompleta = false;
+
+                            // e então marca aquelas no campo de visao
+                            foreach (int i in Core.Core.getCasasVisiveis(TabuleiroVetor, position))
+                                getPeca(i).Image = GetBlinkedImage(i);
+                        }
+                    }
+                }
+            }
             else {
-                TabuleiroVetor[position] = TabuleiroVetor[token_posicao];
-                TabuleiroVetor[token_posicao] = 0;
-                redesenhaTabuleiro();
+                // Se tem alguma peca...
+                if (TabuleiroVetor[position] > 0) {
+                    // primeira desmarca tudo
+                    redesenhaTabuleiro();
+
+                    peca.Image = GetBlinkedImage(position);
+                    JogadaCompleta = false;
+
+                    // e então marca aquelas no campo de visao
+                    foreach (int i in Core.Core.getCasasVisiveis(TabuleiroVetor, position))
+                        getPeca(i).Image = GetBlinkedImage(i);
+                }
+                // Se está vazio
+                else {
+                    // exec jogada
+                    if (Core.Core.getCasasVisiveis(TabuleiroVetor, token_posicao).Contains(position)) {
+                        TabuleiroVetor[position] = TabuleiroVetor[token_posicao];
+                        TabuleiroVetor[token_posicao] = 0;
+                        redesenhaTabuleiro();
+                        }
+                    else {
+                        MessageBox.Show("Jogada proibida.");
+                        return;
+                    }
+                    
+                    if (ExecutaJogada(position))
+                        JogadaCompleta = true;
+                }
             }
 
             // marca a peca clicada
             token_posicao = position;
+
         }
 
         private void PecaHover(object sender, EventArgs e){
@@ -99,6 +236,7 @@ namespace AIWar
 
             // e então marca aquelas no campo de visao
             redesenhaTabuleiro();
+            return;
             foreach (int i in Core.Core.getCasasVisiveis(TabuleiroVetor, getIndex(peca)))
                 getPeca(i).Image = GetBlinkedImage(i);
         }
@@ -108,19 +246,17 @@ namespace AIWar
             peca.BackColor = TransparencyKey;
             peca.Cursor = Cursors.Default;
         }
-        #endregion
+#endregion
 
-		#region "Main Methods"
+#region "Main Methods"
 		protected bool initGame(){
 			Pecas = new GameObjectCollection();
-
-            if (criaPecas())
-				return true;
+            JogoEmAndamento = false;
 			return false;
 		}
 
 		protected bool criaPecas() {
-			for (int i = 0; i < 115; i++) {
+			for (int i = 0; i < MAX_VETOR; i++) {
 				PictureBox e = new PictureBox();
                 e.Name = "peca" + i.ToString();
 				e.Parent = panel1;
@@ -128,7 +264,7 @@ namespace AIWar
 				e.Margin = new Padding(0);
 				e.TabIndex = 0;
 				e.Size = new Size(15, 15);
-				e.Location = new Point(Positions.VetorPeca[i, 0] + panel1.Left, Positions.VetorPeca[i, 1] + panel1.Top);
+                e.Location = new Point(Positions.VetorPeca[i, 0] + panel1.Left, Positions.VetorPeca[i, 1] + panel1.Top);
 
                 // Eventos
                 e.Click += new System.EventHandler(this.PecaClick);
@@ -173,9 +309,141 @@ namespace AIWar
             }
             return null;
         }
-		#endregion
 
-        #region "Auxiliar Methods"
+        private bool ExecutaJogada(int position) {
+            // Prepara p/ a próx jogada: (troca de jogador)
+            // proximo jogador tem jogadas possiveis?
+            bool podeJogar = false;
+
+            if (token_jogador.Cor == Enums.pColor.branca) {
+                for (int j = 0; j < MAX_VETOR; j++) {
+                    if (getPecaCor(j) == Enums.pColor.preta) {
+                        if (Core.Core.getCasasVisiveis(TabuleiroVetor, j).Length > 0)
+                            podeJogar = true;
+                    }
+                }
+            }
+            else if (token_jogador.Cor == Enums.pColor.preta) {
+                for (int j = 0; j < MAX_VETOR; j++) {
+                    if (getPecaCor(j) == Enums.pColor.branca) {
+                        if (Core.Core.getCasasVisiveis(TabuleiroVetor, j).Length > 0)
+                            podeJogar = true;
+                    }
+                }
+            }
+
+            if (podeJogar) {
+                if (token_jogador == jogadores.ElementAt(0))
+                    token_jogador = jogadores.ElementAt(1);
+                else if (token_jogador == jogadores.ElementAt(1))
+                    token_jogador = jogadores.ElementAt(0);
+                lblJogada.Text = token_jogador.name + " - " + token_jogador.Cor.ToString();
+            }
+            else {
+                MessageBox.Show("Sem jogadas disponíveis.");
+                return false;
+            }
+
+            CheckFinishGame();
+
+            return true;
+        }
+
+
+        private bool CheckFinishGame() {
+            int[] pretas = new int[3] {0,0,0};
+            int[] brancas = new int[3] {0,0,0};
+
+            for (int j = 0; j < MAX_VETOR; j++) {
+                switch (TabuleiroVetor[j]) {
+                    case 1: pretas[0]++; break;
+                    case 2: pretas[1]++; break;
+                    case 3: pretas[2]++; break;
+                    case 4: brancas[0]++; break;
+                    case 5: brancas[1]++; break;
+                    case 6: brancas[2]++; break;
+                }
+            }
+
+            if (pretas[0] == 0 || pretas[1] == 0 || pretas[2] == 0) {
+                MessageBox.Show("Jogador de peças brancas ganhou!");
+                JogoEmAndamento = false;
+                return true;
+            }
+            else if (brancas[0] == 0 || brancas[1] == 0 || brancas[2] == 0) {
+                MessageBox.Show("Jogador de peças pretas ganhou!");
+                JogoEmAndamento = false;
+                return true;
+            }
+
+            return false;
+        }
+
+    #region "Catpura"
+        private bool PecaCapturavel(int pos, Enums.pColor cor) {
+            int qtd = 0;
+            int cargasSoma = 0;
+
+            foreach (int i in Core.Core.getPecasVisiveis(TabuleiroVetor, pos)) {
+                cargasSoma += getParticulaCarga(i);
+                if (getPecaCor(i) == Enums.pColor.preta && cor == Enums.pColor.branca)
+                    qtd++;
+                else if (getPecaCor(i) == Enums.pColor.branca && cor == Enums.pColor.preta)
+                    qtd++;
+            }
+
+            return (qtd > 1) && (cargasSoma == 0);
+        }
+        
+        private bool PecaQueCaptura(int pos){
+            foreach(int i in Core.Core.getPecasVisiveis(TabuleiroVetor, pos)){
+                if (PecaCapturavel(i, getPecaCor(i)))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool DeveCapturar(){
+           
+            if (token_jogador.Cor == Enums.pColor.branca) {
+                for (int j = 0; j < MAX_VETOR; j++) {
+                    if (getPecaCor(j) == Enums.pColor.preta) {
+                        int qtd = 0;
+                        int cargasSoma = 0;
+
+                        foreach (int i in Core.Core.getPecasVisiveis(TabuleiroVetor, j)) {
+                            cargasSoma += getParticulaCarga(i);
+                            if (getPecaCor(i) == Enums.pColor.branca) qtd++;
+                        }
+
+                        if ((qtd > 1) && (cargasSoma == 0))
+                            return true;
+                    }
+                }
+            }
+            else if (token_jogador.Cor == Enums.pColor.preta) {
+                for (int j = 0; j < MAX_VETOR; j++) {
+                    if (getPecaCor(j) == Enums.pColor.branca) {
+                        int qtd = 0;
+                        int cargasSoma = 0;
+
+                        foreach (int i in Core.Core.getPecasVisiveis(TabuleiroVetor, j)) {
+                            cargasSoma += getParticulaCarga(i);
+                            if (getPecaCor(i) == Enums.pColor.preta) qtd++;
+                        }
+                        if ((qtd > 1) && (cargasSoma == 0))
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    #endregion
+#endregion
+
+    #region "Auxiliar Methods"
         /// <summary>
         /// Retorna a Peça que está no índice i.
         /// </summary>
@@ -195,21 +463,29 @@ namespace AIWar
             return Convert.ToInt32(peca.Name.Replace("peca", ""));
         }
 
+        private Core.Enums.pColor getPecaCor(int i) {
+            if (TabuleiroVetor[i] > 0 && TabuleiroVetor[i] < 4)
+                return Enums.pColor.preta;
+            else if(TabuleiroVetor[i] > 3)
+                return Enums.pColor.branca;
+            return Enums.pColor.undefined;
+        }
+
         /// <summary>
         /// Retorna a carga de uma particula indicada pelo tipo de peça no tabuleiro.
         /// </summary>
         /// <returns></returns>
         private int getParticulaCarga(int i) {
-            switch (i) {
-                case 1: case 4: return -1;
-                case 2: case 5: return 1;
+            switch (TabuleiroVetor[i]) {
+                case 2: case 5: return -1;
+                case 3: case 6: return 1;
             }
             return 0;
         }
 
         private bool redesenhaTabuleiro()
         {
-            for (int i = 0; i < 115; i++)
+            for (int i = 0; i < MAX_VETOR; i++)
             {
                 PictureBox ipeca = (PictureBox)this.Controls.Find("peca" + i.ToString(), true).First();
                 if (ipeca != null)
@@ -232,7 +508,6 @@ namespace AIWar
 
             return lista;
         }
-        #endregion
-
+    #endregion
     }
 }
